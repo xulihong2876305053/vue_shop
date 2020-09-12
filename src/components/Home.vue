c962ec6b9a8b79aa86f34f4a492ae3b00317b9b7
xulihong2876305053 <2876305053@qq.com>
2020-09-12 22:10:03 +0800

    第一天完成

---
+++ src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <el-button type='info' @click='logout'>退出</el-button> 
+  </div>
+</template>
+<script>
+  export default {
+    name: '',
+    data() {
+      return {}
+    },
+    methods: {
+      logout() {
+        window.sessionStorage.clear()
+        this.$router.push('/login')
+      }
+    },
+    components: {
+
+    }
+  }
+</script>
+<style scoped>
+
+</style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,10 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
+
+import '../assets/font/iconfont.css'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.prototype.$message = Message
